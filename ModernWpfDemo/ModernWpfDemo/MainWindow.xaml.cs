02744265fb74e17a4526ac13d0d996ee9cbfdf27
Matteo Pagani <matteo.pagani@microsoft.com>
2020-02-13 10:47:58 +0400

    Updated final version of the demo

--- ModernWpfDemo/ModernWpfDemo/MainWindow.xaml.cs
+++ ModernWpfDemo/ModernWpfDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Windows.Devices.Geolocation;
 
 namespace ModernWpfDemo
 {
@@ -11,14 +12,14 @@ namespace ModernWpfDemo
 
         private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            //Geolocator geolocator = new Geolocator() { DesiredAccuracyInMeters = 5 };
-            //Geoposition pos = await geolocator.GetGeopositionAsync();
-            //string longitude = pos.Coordinate.Point.Position.Longitude.ToString();
-            //string latitude = pos.Coordinate.Point.Position.Latitude.ToString();
-            //LongBlock.Text = longitude;
-            //LatBlock.Text = latitude;
+            Geolocator geolocator = new Geolocator() { DesiredAccuracyInMeters = 5 };
+            Geoposition pos = await geolocator.GetGeopositionAsync();
+            string longitude = pos.Coordinate.Point.Position.Longitude.ToString();
+            string latitude = pos.Coordinate.Point.Position.Latitude.ToString();
+            LongBlock.Text = longitude;
+            LatBlock.Text = latitude;
 
-            //await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
+            await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
         }
     }
 }
